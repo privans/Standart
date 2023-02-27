0448f8db42276ef6ccd369c4dfc60de8975baa35
privans <116141079+privans@users.noreply.github.com>
2023-02-28 07:08:08 +0800

    reuse code

--- jest.config.ts
+++ jest.config.ts
@@ -1,2 +1,8 @@
 /*
- * For a detailed explanation regarding each
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+	// All imported modules in your tests should be mocked automatically
+	// auto
