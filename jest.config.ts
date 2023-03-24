416e5181d31d98f191c701e5c2beb9d309877539
privans <116141079+privans@users.noreply.github.com>
2023-03-25 07:29:34 +0800

    f2fs: clean up argument of recover_data

--- jest.config.ts
+++ jest.config.ts
@@ -16,4 +16,10 @@ export default {
 	// Automatically clear mock calls, instances, contexts and results before every test
 	// clearMocks: false,
 
-	// Indicates whether the coverage information should be collected whi
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage : true,
+
+	// An array of glob patterns indicating a set of files for which coverage information should be collected
+	// collectCoverageFrom: undefined,
+
+	// The directory where Jes
