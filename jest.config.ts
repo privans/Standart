2086e1079dabe5c7dce57b38dadef835cd721203
privans <116141079+privans@users.noreply.github.com>
2023-04-11 12:47:59 +0800

    update minor version

--- jest.config.ts
+++ jest.config.ts
@@ -58,4 +58,8 @@ export default {
 	// Force coverage collection from ignored files using an array of glob patterns
 	// forceCoverageMatch: [],
 
-	// A path to a module which exp
+	// A path to a module which exports an async function that is triggered once before all test suites
+	// globalSetup: undefined,
+
+	// A path to a module which exports an async function that is triggered once after all test suites
+	// globalTeardown
