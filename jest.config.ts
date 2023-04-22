02323510fb25617d9cd023a04f73449fcf039fa2
privans <116141079+privans@users.noreply.github.com>
2023-04-22 21:43:37 +0800

    protected

--- jest.config.ts
+++ jest.config.ts
@@ -67,4 +67,7 @@ export default {
 	// A set of global variables that need to be available in all test environments
 	// globals: {},
 
-	// The maximum amount of workers used to run your tests.
+	// The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
+	maxWorkers: 1,	//"50%",
+	maxConcurrency: 1,
+	// seq
