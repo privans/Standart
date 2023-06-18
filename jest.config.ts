06756e566437bbea2d521c764b990ecd636b7441
privans <116141079+privans@users.noreply.github.com>
2023-06-18 10:02:46 +0800

    * tests

--- jest.config.ts
+++ jest.config.ts
@@ -188,4 +188,7 @@ export default {
 	// An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
 	// unmockedModulePathPatterns: undefined,
 
-	//
+	// Indicates whether each individual test should be reported during the run
+	verbose: true,
+
+	// An array of regexp patterns that are matched against all source file paths before re-running tests in
