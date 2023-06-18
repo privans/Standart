1f7abe40f01bd26163fce7e4fe4fafc3cde7929b
privans <116141079+privans@users.noreply.github.com>
2023-06-18 09:32:14 +0800

    updated list

--- jest.config.ts
+++ jest.config.ts
@@ -176,4 +176,9 @@ export default {
 
 	// A map from regular expressions to paths to transformers
 	// transform : {
-	// 	`^.+\\.t
+	// 	`^.+\\.tsx?$` : `ts-jest`,
+	// },
+
+	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+	// transformIgnorePatterns: [
+	//   "/node_modules/"
