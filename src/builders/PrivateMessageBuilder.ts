bd2dd8f4fbcefb02544f649827457ee672e65923
privans <116141079+privans@users.noreply.github.com>
2023-08-09 12:48:50 +0800

    Removed commented out legacy code.

--- src/builders/PrivateMessageBuilder.ts
+++ src/builders/PrivateMessageBuilder.ts
@@ -1,2 +1,6 @@
 import { ChatMessage, SendMessageRequest } from "../models/messages/SendMessageRequest";
-import _ from "lo
+import _ from "lodash";
+import { isAddress, isHexString } from "ethers";
+import { VaChatRoomEntityItem } from "../validators/VaChatRoomEntityItem";
+import { Web3Digester, Web3Signer } from "debeem-id";
+import { PrivateMessage
