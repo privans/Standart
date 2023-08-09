1a7cdebc93486c2e0686cfe47a1bc88fe3fde187
privans <116141079+privans@users.noreply.github.com>
2023-08-09 16:29:41 +0800

    Updating dev dependencies

--- src/builders/PrivateMessageBuilder.ts
+++ src/builders/PrivateMessageBuilder.ts
@@ -3,4 +3,12 @@ import _ from "lodash";
 import { isAddress, isHexString } from "ethers";
 import { VaChatRoomEntityItem } from "../validators/VaChatRoomEntityItem";
 import { Web3Digester, Web3Signer } from "debeem-id";
-import { PrivateMessage
+import { PrivateMessageCrypto } from "../cryptos/PrivateMessageCrypto";
+import { ChatRoomEntityItem } from "../entities/ChatRoomEntity";
+import { BaseMessageBuilder } from "./BaseMessageBuilder";
+
+
+/**
+ * 	@class
+ */
+expo
