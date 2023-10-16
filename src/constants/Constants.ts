ca47e3f3704cbb78113e16f983b26c042c475891
privans <116141079+privans@users.noreply.github.com>
2023-10-16 14:32:57 +0800

    ASoC: wm2200: Fix setting dai format in wm2200_set_fmt

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -3,4 +3,10 @@ export class Constants
 	static minLengthUserName : number	= 3;
 	static maxLengthUserName : number	= 128;
 
-	static minLengthUse
+	static minLengthUserAvatar : number	= 18;
+	static maxLengthUserAvatar : number	= 256;
+
+	static minLengthRoomName : number	= 3;
+	static maxLengthRoomName : number	= 64;
+
+	static minLengthRoomId : number		= 42;
