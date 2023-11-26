b8df67dab3974b128d5cd783ae9d2a7e275a15a7
privans <116141079+privans@users.noreply.github.com>
2023-11-26 13:33:35 +0800

    get_dvb_firmware: fix download site for tda10046 firmware

--- src/models/messages/PullMessageResponse.ts
+++ src/models/messages/PullMessageResponse.ts
@@ -1,2 +1,16 @@
 import { SendMessageRequest } from "./SendMessageRequest";
-import { BaseRes
+import { BaseResponse } from "../BaseResponse";
+
+/**
+ * 	response
+ */
+export interface QueueMessageItem
+{
+	channel : string,
+	timestamp : number,
+	data : SendMessageRequest,
+}
+
+export interface PullMessageResponse extends BaseResponse
+{
+	total
