0d2c0bb12617157f1c9ce4296ed5f705b4c05745
privans <116141079+privans@users.noreply.github.com>
2023-11-30 16:26:31 +0800

    Change function name

--- src/models/rooms/InviteRequest.ts
+++ src/models/rooms/InviteRequest.ts
@@ -1,3 +1,14 @@
 import { ChatRoomEntityItem } from "../../entities/ChatRoomEntity";
 
-e
+export interface InviteRequest
+{
+	version : string;
+	header : string;
+	chatRoom : ChatRoomEntityItem;
+}
+
+export const clientInviteRequestInitV1 : InviteRequest =
+{
+	version : '1.0',
+	header : 'CHAT_INVITE',
+	chatRoom : {} as ChatRoomEntityItem,
