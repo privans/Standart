b44282d986df002775837b9ab32c22e2b72b2804
privans <116141079+privans@users.noreply.github.com>
2024-01-10 06:44:45 +0800

    feat(update): minor changes

--- src/validators/VaExistRoomRequest.ts
+++ src/validators/VaExistRoomRequest.ts
@@ -1,2 +1,13 @@
 import { VaChatRoomEntityItem } from "./VaChatRoomEntityItem";
-import { ExistRoomRequest } f
+import { ExistRoomRequest } from "../models/rooms/ExistRoomRequest";
+
+/**
+ * 	@class
+ */
+export class VaExistRoomRequest
+{
+	/**
+	 *	@param existRoomRequest	{ExistRoomRequest}
+	 *	@returns {string | null}
+	 */
+	static val
