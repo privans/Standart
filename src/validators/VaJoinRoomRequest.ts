e88ea7eebb4a44c964d05909ff8fbf9d0f3891e2
privans <116141079+privans@users.noreply.github.com>
2024-01-10 09:05:34 +0800

    Added initial files.

--- src/validators/VaJoinRoomRequest.ts
+++ src/validators/VaJoinRoomRequest.ts
@@ -1,1 +1,10 @@
-import { JoinRoomRequ
+import { JoinRoomRequest } from "../models/rooms/JoinRoomRequest";
+import { VaChatRoomEntityItem } from "./VaChatRoomEntityItem";
+
+/**
+ * 	@class
+ */
+export class VaJoinRoomRequest
+{
+	/**
+	 *	@param joinRoo
