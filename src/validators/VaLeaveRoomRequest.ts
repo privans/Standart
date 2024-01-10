b4fb8de7e38f7bd7dc428997965d5968b76eddc1
privans <116141079+privans@users.noreply.github.com>
2024-01-10 14:07:46 +0800

    cleanup.

--- src/validators/VaLeaveRoomRequest.ts
+++ src/validators/VaLeaveRoomRequest.ts
@@ -6,3 +6,25 @@ import { VaChatRoomEntityItem } from "./VaChatRoomEntityItem";
  * 	@class
  */
 export class VaLeaveRoomRequest
+{
+	/**
+	 *	@param leaveRequest	{LeaveRoomRequest}
+	 *	@returns {string | null}
+	 */
+	static validateLeaveRoomRequest( leaveRequest : LeaveRoomRequest ) : string | null
+	{
+		if ( ! leaveRequest )
+		{
+			return `invalid leaveRequest`;
+		}
+
+		const errorRoomId : string | null = VaChatRoomEntityItem.isValidRoomId( leaveRequest.roomId );
+		if ( null !== errorRoomId )
+		{
+			return errorRoomId;
+		}
+
+		//	...
+		return null;
+	}
+}
---
+++ src/validators/VaPullMessageRequest.ts
@@ -0,0 +1,8 @@
+import { PullMessageRequest } from "../models/messages/PullMessageRequest";
+import { VaChatRoomEntityItem } from "./VaChatRoomEntityItem";
+import _ from "lodash";
+
+/**
+ * 	@class
+ */
+export cla
