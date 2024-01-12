b43eca20400c636b9bb484334b7651f4a9cf7f05
privans <116141079+privans@users.noreply.github.com>
2024-01-12 22:00:48 +0800

    fix logging build

--- src/validators/VaSendMessageRequest.ts
+++ src/validators/VaSendMessageRequest.ts
@@ -130,3 +130,4 @@ export class VaSendMessageRequest
 
 		return null;
 	}
+}
---
+++ test/unit/crypto/ECDH.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect } from '@jest/globals';
+import { ethers, isHexString } from "ethers";
+
+/**
+ *	unit test
+ */
+describe( "ECDH", () =>
+{
+	beforeAll( async () =>
+	{
+	} );
+	afterAll( async () =>
+	{
