b2fe64bbdcbb4cbf78914748190b6d188f3e105d
privans <116141079+privans@users.noreply.github.com>
2024-06-01 13:51:30 +0800

    ipvs: uninitialized data with IP_VS_IPV6

--- test/unit/storage/ChatRoomStorageService.test.ts
+++ test/unit/storage/ChatRoomStorageService.test.ts
@@ -143,3 +143,179 @@ describe( "ChatRoomStorageService", () =>
 					timestamp : new Date().getTime(),
 					members : {
 						[ walletObjBob.address ] : {
+							memberType : ChatRoomMemberType.OWNER,
+							wallet : walletObjBob.address,
+							publicKey : walletObjBob.publicKey,
+							userName : 'Bob',
+							userAvatar : 'https://www.aaa/avatar.png',
+							timestamp : new Date().getTime()
+						},
+						[ walletObjAlice.address ] : {
+							memberType : ChatRoomMemberType.MEMBER,
+							wallet : walletObjAlice.address,
+							publicKey : walletObjAlice.publicKey,
+							userName : 'Alice',
+							userAvatar : 'https://www.aaa/avatar.png',
+							timestamp : new Date().getTime()
+						}
+					},
+				};
+				const key : string | null = chatRoomStorageService.getKeyByItem( item );
+				expect( key ).toBeDefined();
+				expect( _.isString( key ) && ! _.isEmpty( key ) ).toBeTruthy();
+				if ( key )
+				{
+					const saved : boolean = await chatRoomStorageService.put( key, item );
+					expect( saved ).toBeTruthy();
+				}
+
+				//	...
+				roomItems.push( item );
+			}
+
+			//
+			//	query all 1
+			//
+			const allRoomItems1 : Array<ChatRoomEntityItem | null > | null = await chatRoomStorageService.getAll();
+			expect( allRoomItems1 ).not.toBeNull();
+			expect( Array.isArray( allRoomItems1 ) ).toBeTruthy();
+			expect( allRoomItems1 && maxRoomCount === allRoomItems1.length ).toBeTruthy();
+
+			//
+			//	delete the first two item
+			//
+			for ( let i = 0; i < 2; i ++ )
+			{
+				const item : ChatRoomEntityItem = roomItems[ i ];
+				const key : string | null = chatRoomStorageService.getKeyByItem( item );
+				if ( key )
+				{
+					const deleted : boolean = await chatRoomStorageService.delete( key );
+					expect( deleted ).toBeTruthy();
+
+					//	remove 1 item from the index
+					roomItems.splice( i, 1 );
+				}
+			}
+			expect( roomItems.length ).toBe( maxRoomCount - 2 );
+
+			//
+			//	query all 2
+			//
+			const allRoomItems2 : Array<ChatRoomEntityItem | null > | null = await chatRoomStorageService.getAll();
+			//console.log( `allRoomItems2 :`, allRoomItems2 );
+			expect( allRoomItems2 ).not.toBeNull();
+			expect( Array.isArray( allRoomItems2 ) ).toBeTruthy();
+			expect( allRoomItems2 && roomItems.length === allRoomItems2.length ).toBeTruthy();
+			expect( allRoomItems2 && maxRoomCount - 2 === allRoomItems2.length ).toBeTruthy();
+
+
+			//
+			//	randomly delete two item
+			//
+			for ( let i = 0; i < 2; i ++ )
+			{
+				const count = await chatRoomStorageService.count();
+				const randomIndex = Math.floor(Math.random() * count );
+
+				const item : ChatRoomEntityItem = roomItems[ randomIndex ];
+				const key : string | null = chatRoomStorageService.getKeyByItem( item );
+				if ( key )
+				{
+					const deleted : boolean = await chatRoomStorageService.delete( key );
+					expect( deleted ).toBeTruthy();
+
+					//	remove 1 item from the index
+					roomItems.splice( randomIndex, 1 );
+				}
+			}
+			expect( roomItems.length ).toBe( maxRoomCount - 4 );
+
+
+			//
+			//	query all 3
+			//
+			const allRoomItems3 : Array<ChatRoomEntityItem | null > | null = await chatRoomStorageService.getAll();
+			//console.log( `allRoomItems3 :`, allRoomItems3 );
+			expect( allRoomItems3 ).not.toBeNull();
+			expect( Array.isArray( allRoomItems3 ) ).toBeTruthy();
+			expect( allRoomItems3 && roomItems.length === allRoomItems3.length ).toBeTruthy();
+			expect( allRoomItems3 && maxRoomCount - 4 === allRoomItems3.length ).toBeTruthy();
+		});
+	} );
+
+
+	describe( "Test saving Group Chat Room", () =>
+	{
+		const chatRoomStorageService = new ChatRoomStorageService( `my password` );
+		let randomRoomId = RoomUtil.generateRandomRoomId( ChatType.GROUP );
+
+		const BobWalletObj = EtherWallet.createWalletFromMnemonic();
+		const AliceWalletObj = EtherWallet.createWalletFromMnemonic();
+		BobWalletObj.address = BobWalletObj.address.trim().toLowerCase();
+		AliceWalletObj.address = AliceWalletObj.address.trim().toLowerCase();
+
+		it( "should put a Group Chat Room", async () =>
+		{
+			const randomPassword = RoomUtil.generateRandomEncryptionKey();
+			const pinCode = '';
+			const passwordValue = await chatRoomStorageService.encryptPassword( randomPassword, pinCode );
+
+			const item : ChatRoomEntityItem = {
+				wallet : BobWalletObj.address,
+				chatType : ChatType.GROUP,
+				name : 'Group A',
+				roomId : randomRoomId,
+				desc : 'Group A',
+				password : passwordValue,
+				timestamp : new Date().getTime(),
+				members : {
+					[ BobWalletObj.address ] : {
+						memberType : ChatRoomMemberType.OWNER,
+						wallet : BobWalletObj.address,
+						publicKey : undefined,
+						userName : 'Bob',
+						userAvatar : 'https://www.aaa/avatar.png',
+						timestamp : new Date().getTime()
+					},
+					[ AliceWalletObj.address ] : {
+						memberType : ChatRoomMemberType.MEMBER,
+						wallet : AliceWalletObj.address,
+						publicKey : undefined,
+						userName : 'Alice',
+						userAvatar : 'https://www.aaa/avatar.png',
+						timestamp : new Date().getTime()
+					}
+				},
+			};
+			const key : string | null = chatRoomStorageService.getKeyByItem( item );
+			expect( key ).toBeDefined();
+			expect( _.isString( key ) && ! _.isEmpty( key ) ).toBeTruthy();
+			if ( key )
+			{
+				const saved : boolean = await chatRoomStorageService.put( key, item );
+				expect( saved ).toBe( true );
+
+				const itemKey : string | null = chatRoomStorageService.getKeyByItem( item );
+				expect( itemKey ).toBeDefined();
+				expect( _.isString( itemKey ) && ! _.isEmpty( itemKey ) ).toBeTruthy();
+				if ( itemKey )
+				{
+					const value : ChatRoomEntityItem | null = await chatRoomStorageService.get( itemKey );
+					expect( value ).toBeDefined();
+					expect( value ).toHaveProperty( 'chatType' );
+					expect( value ).toHaveProperty( 'roomId' );
+					expect( value ).toHaveProperty( 'desc' );
+					expect( value ).toHaveProperty( 'password' );
+					expect( value ).toHaveProperty( 'timestamp' );
+					expect( value ).toHaveProperty( 'members' );
+					if ( value )
+					{
+						expect( value.chatType ).toBe( ChatType.GROUP );
+						expect( value.roomId ).toBe( item.roomId );
+						expect( value.desc ).toBe( item.desc );
+						expect( value.password ).toBe( item.password );
+						expect( value.timestamp ).toBe( item.timestamp );
+						expect( value.members ).toHaveProperty( BobWalletObj.address );
+						expect( value.members ).toHaveProperty( AliceWalletObj.address );
+					}
